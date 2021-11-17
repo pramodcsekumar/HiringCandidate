bedaa4faf3936d6d1b82b1792421865ef5f69ebe
pramodcsekumar <44620245+pramodcsekumar@users.noreply.github.com>
2021-11-17 14:20:06 +0530

    Add files via upload
    Initial commit.

---
+++ hiring/src/test/java/com/walmart/hiring/HiringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.hiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
